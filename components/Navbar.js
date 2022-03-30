4c5d5550680358877da2e34908bc69889ffd96f5
Gourav Rana <85408869+GouravRana807@users.noreply.github.com>
2022-03-30 19:43:43 +0530

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,41 +1,7 @@
-// import React from 'react';
-// import styles from '../styles/Home.module.css';
-// import Link from 'next/link';
-
-// const Navbar = () => {
-//   return (
-//     <nav className={styles.mainnav}>
-//       <ul>
-//         <Link href="/">
-//           <a>
-//             <li>Home</li>
-//           </a>
-//         </Link>
-//         <Link href="/about">
-//           <a>
-//             <li>About</li>
-//           </a>
-//         </Link>
-//         <Link href="/blog">
-//           <a>
-//             <li>Blog</li>
-//           </a>
-//         </Link>
-//         <Link href="/contact">
-//           <a>
-//             <li>Contact</li>
-//           </a>
-//         </Link>
-//       </ul>
-//     </nav>
-//   );
-// };
-
-// export default Navbar;
-
 import React from 'react';
 import styles from '../styles/Home.module.css';
 import Link from 'next/link';
+
 const Navbar = () => {
   return (
     <nav className={styles.mainnav}>
@@ -66,3 +32,4 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
